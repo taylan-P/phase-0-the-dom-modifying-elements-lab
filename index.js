6b4b949401bbc22c98aa802a2ab69374d76133f1
TaylanPostalci <tay.postal@gmail.com>
2022-09-08 15:48:33 -0400

    elementLab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove (main);
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Taylan is the champion"
+document.body.append(newHeader);
